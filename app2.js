9388d2d95caa82b4fd55e64c804874380c61c5b1
Jethro Glaudin <jethroglaudin.student@careerdevs.com>
2019-04-11 23:40:04 +0000

    lesson04

--- app2.js
+++ app2.js
@@ -1,4 +1,16 @@
-var titles = document.getElementsByClassName('title');
-Array.from(titles).forEach(function(item){
-  console.log(item);
+const wmf = document.querySelector('#book-list li:nth-child(2) .name');
+//console.log(wmf);
+
+
+// Returns one element
+var books =document.querySelector('#book-list li .name');
+// console.log(books)
+
+// Returns a collection of elements
+books =document.querySelectorAll('#book-list li .name');
+
+// console.log(books);
+
+Array.from(books).forEach(function(book){
+  console.log(book);
 });
