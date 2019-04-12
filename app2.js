da9c2cccbb3945e7abc12f8fe964525f7b78ece3
Jethro Glaudin <jethroglaudin.student@careerdevs.com>
2019-04-12 22:23:33 +0000

    Lesson11 completed

--- app2.js
+++ app2.js
@@ -7,3 +7,15 @@ list.addEventListener('click', function(e){
 
   }
 });
+
+
+//  add book list
+
+const addForm = document.forms["add-book"];
+
+addForm.addEventListener('submit', function(e){
+  e.preventDefault()
+  const value = addForm.querySelector('input[type= "text"]').value;
+
+  console.log(value);
+});
