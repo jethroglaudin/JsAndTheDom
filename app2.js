176ec2915009261ade6f27706162a1831d7c8f5f
Jethro Glaudin <jethroglaudin.student@careerdevs.com>
2019-04-12 23:25:49 +0000

    lesson12 completed

--- app2.js
+++ app2.js
@@ -1,21 +1,29 @@
 const list = document.querySelector('#book-list ul');
 // delete books
-list.addEventListener('click', function(e){
-  if (e.target.className == 'delete'){
+list.addEventListener('click', function(e) {
+  if (e.target.className == 'delete') {
     const li = e.target.parentElement;
     list.removeChild(li);
-
   }
 });
-
-
 //  add book list
-
 const addForm = document.forms["add-book"];
-
-addForm.addEventListener('submit', function(e){
-  e.preventDefault()
+addForm.addEventListener('submit', function(e) {
+  e.preventDefault();
   const value = addForm.querySelector('input[type= "text"]').value;
-
   console.log(value);
+
+  // create elements
+  const li = document.createElement('li');
+  const bookName = document.createElement('span');
+  const deleteBtn = document.createElement('span');
+
+  //add content
+  deleteBtn.textContent = 'delete';
+  bookName.textContent = value;
+
+  // append to document
+  li.appendChild(bookName);
+  li.appendChild(deleteBtn);
+  list.appendChild(li);
 });
