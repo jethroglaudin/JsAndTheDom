529f4a1d301c4ab14c5f4f72b3dbd7bccc00c857
Jethro Glaudin <jethroglaudin.student@careerdevs.com>
2019-04-12 21:41:16 +0000

    lesson10 completed

--- app2.js
+++ app2.js
@@ -1,19 +1,9 @@
-var btns = document. querySelectorAll('#book-list .delete');
-
-Array.from(btns).forEach(function(btn){
-  btn.addEventListener('click', function(e){
-
+const list = document.querySelector('#book-list ul');
+// delete books
+list.addEventListener('click', function(e){
+  if (e.target.className == 'delete'){
     const li = e.target.parentElement;
+    list.removeChild(li);
 
-    li.parentNode.removeChild(li);
-
-  });
-});
-
-const link = document.querySelector('#page-banner a');
-
-link.addEventListener('click', function(e){
-
-  e.preventDefault();
-  console.log('navigation to', e.target.textContent, 'was prevented');
+  }
 });
